6a5728c3443e2d5dd8ad8f999da1ffa08e71d7eb
Akarshan Kumar <akarshan2602@gmail.com>
2024-09-09 05:02:54 +0530

    fixed a small bug in userRouting

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+
+
 
 app.listen(port, () => console.log(`Server running on post: ${port}`));
