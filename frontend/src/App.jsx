732a4bd455dc422db9910037fb325e2b152b498a
akarshan2602 <akarshan.k2602@gmail.com>
2024-09-17 03:26:12 +0530

    building navbar , not displaying in start! FIX IT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,23 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
+import {Outlet} from 'react-router-dom';
+import Navigation from './pages/Auth/Navigation';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className='py-3'>
+        <Outlet />
+      </main>
+
+    </>
   )
 }
 
